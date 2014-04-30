e46effe06e88b16447ba5a6c8acc4959458ebf72
Jos De Roo <josderoo@gmail.com>
2014-04-30 12:43:07 +0200

    added

---
+++ make.cmd
@@ -0,0 +1,2 @@
+@echo off
+call eye --nope medicalEntity-to-rdfa.n3 medicalEntityOntology.ttl --strings > medicalEntity.rdfa
---
+++ make.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+eye --nope medicalEntity-to-rdfa.n3 medicalEntityOntology.ttl --strings > medicalEntity.rdfa
